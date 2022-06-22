4ec46c0b6fe89e659d7ce1cfb5e01e6e1e1be025
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-21 22:04:24 -0400

    delete btn is working

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from crypt import methods
-from flask import Flask, render_template, request, redirect, url_for,session
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import pymongo
-MONGODB_URI ='mongodb+srv://fuad:Fuad@todopy.z78my1s.mongodb.net/?retryWrites=true&w=majority'
+from bson.objectid import ObjectId
+MONGODB_URI = 'mongodb+srv://fuad:Fuad@todopy.z78my1s.mongodb.net/?retryWrites=true&w=majority'
 client = pymongo.MongoClient(MONGODB_URI)
 db = client.TODOpy
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 todo_list = list()
-@app.route('/' , methods= ['GET','POST']) # this decorator create the home route
+
+
+# this decorator create the home route
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    show_data()
+    # show_data()
+    tasks = db.tasks.find()
     if request.method == 'GET':
-        return render_template('index.html', title='TODOpy',todo_list=todo_list)
+        return render_template('index.html', title='TODOpy', tasks=tasks)
     if request.method == 'POST':
         topic = request.form['topic']
         due_date = request.form['date']
@@ -21,20 +26,24 @@ def home():
             db.tasks.insert_one({'topic': topic, 'due_date': due_date})
             return redirect('/')
         else:
-            return render_template('index.html', title='TODOpy',todo_list=todo_list)
+            return render_template('index.html', title='TODOpy', tasks=tasks)
 
-@app.route('/update', methods= ['GET','POST'])
+#methods=['GET', 'POST']
+
+
+@app.route('/update', methods=['GET', 'POST'])
 def update():
-    return render_template('update.html',title='TODOpy')
-    
-def show_data():
-    collection = db['tasks']
-    datas = collection.find({})
-    for data in datas:
-        if (data['topic'],data['due_date']) not in todo_list:
-            todo_list.append((data['topic'],data['due_date']))
+    return render_template('update.html', title='TODOpy')
+
+
+@app.route('/<id>/delete/', methods=['POST'])
+def delete(id):
+    db.tasks.delete_one({"_id": ObjectId(id)})
+    return redirect('/')
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+# onclick="return confirm('Are you sure you want to delete this entry?')">
